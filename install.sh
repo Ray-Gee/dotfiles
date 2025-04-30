74db5c272a8611da767cedc49ba64eb0d06e3a54
RyuichiUeda <hsatlefp@gmail.com>
2025-04-30 23:03:38 +0900

    install.shの.zprofileパスをユーザーのホームディレクトリに変更

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [ ! -f /usr/local/bin/brew ]; then
     # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/uedaryuuichi/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 
 # if [ ! -d ~/works/dotfiles ]; then
 #     cd ~
